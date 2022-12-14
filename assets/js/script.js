fcba993a08aa8e71293d258d0235f6e0145f72c9
miacias <miaciasullo@gmail.com>
2022-12-14 17:35:33 -0500

    beggining to build FOR LOOP that inserts question data from questionBank array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,42 @@
-const boop = document.getElementById("boop");
-boop.volume = 0.2;
+// BONUS: consider making a randomizer function that cycles through quetions so that questions can be scoped locally (i.e. made private). how to get it to not repeat questions twice?
+// BONUS: add a counter to question numbers so each question is counter +=1! this way it won't print question# but count on its own (in case questions are randomized)
 
-// consider making a randomizer function that cycles through quetions so that questions can be scoped locally (i.e. made private). how to get it to not repeat questions twice?
 const questionBank = [question1, question2, question3, question4];
 
 var question1 = {
     question: "Inside the HTML document, where do you place your JavaScript code?",
-    response1: "in the <head> element",
-    response2: "in the <link> element",
-    response3: "in the <footer> element",
-    response4: "in the <script> element", // true
+    responseA: "in the <head> element",
+    responseB: "in the <link> element",
+    responseC: "in the <footer> element",
+    responseD: "in the <script> element", // true
+    // question1.response4 = true;
     correctFeedback: "Nice job! JavaScript is placed inside the <script> element of the HTML Document.",
     incorrectFeedback: "JavaScript is stored in a <script> element in HTML."
 }
 var question2 = {
     question: "What operator is used to assign a value to a declared variable?",
-    response1: "question mark (?)",
-    response2: "double equality (==)",
-    response3: "single equality (=)", // true
-    response4: "colon (:)",
+    responseA: "question mark (?)",
+    responseB: "double equality (==)",
+    responseC: "single equality (=)", // true
+    responseD: "colon (:)",
     correctFeedback: "Awesome! The correct way to assign a variable is with an equal sign (=).",
     incorrectFeedback: "Variables can be assigned a value with an equal sign (=)."
 }
 var question3 = {
     question: "What are the six primitive data types in JavaScript?",
-    response1: "sentence, float, data, bigInt, symbol, undefined",
-    response2: "sentence, int, truthy, bigInt, symbol, undefined",
-    response3: "string, number, boolean, bigInt, symbol, undefined", // true
-    response4: "string, num, falsy, bigInt, symbol, undefined",
+    responseA: "sentence, float, data, bigInt, symbol, undefined",
+    responseB: "sentence, int, truthy, bigInt, symbol, undefined",
+    responseC: "string, number, boolean, bigInt, symbol, undefined", // true
+    responseD: "string, num, falsy, bigInt, symbol, undefined",
     correctFeedback: "Stellar! JavaScript has a total of six primitive data types: string, number, boolean, bigInt, symbol, undefined.",
     incorrectFeedback: "JS's six primitive data types are: string, number, boolean, bigInt, symbol, undefined."
 }
 var question4 = {
     question: "What is the difference between && and || ?",
-    response1: "The logical operator && returns true if ONE expression is true while the logical operator || returns true if BOTH expressions return true.",
-    response2: "The logical operator && returns true if BOTH expressions are true while the logical operator || returns true if ONE expression or the other returns true.", // true
-    response3: "The logical operator && returns true if BOTH expressions are true while the logical operator || returns false if ONE expression or the other returns true.",
-    response4: "The logical operator && returns true if NONE of the expressions are true while the logical operator || returns true if ONE expression or the other returns true.",
+    responseA: "The logical operator && returns true if ONE expression is true while the logical operator || returns true if BOTH expressions return true.",
+    responseB: "The logical operator && returns true if BOTH expressions are true while the logical operator || returns true if ONE expression or the other returns true.", // true
+    responseC: "The logical operator && returns true if BOTH expressions are true while the logical operator || returns false if ONE expression or the other returns true.",
+    responseD: "The logical operator && returns true if NONE of the expressions are true while the logical operator || returns true if ONE expression or the other returns true.",
     correctFeedback: "High-five! The logical operator && returns true if both expressions are true while the logical operator || returns true if one expression or the other returns true.",
     incorrectFeedback: "AND (&&) means both expressions are true while OR (||) means only one expression is true."
 }
@@ -44,6 +44,7 @@ var question4 = {
 // JS Activity 7: shows how to create HTML element tags with text and attach as children in document.
 
 // access to HTML sections
+
 var startButton = document.querySelector(".start-button");
 var openingPage = document.querySelector("#opening-page");
 var quizPage = document.querySelector("#quiz-page");
@@ -92,20 +93,36 @@ function timeRemaining() { // only visible after clicking Start Button
     }, 1000);
 }
 
+// use for loop to insert question properties based on questionBank array. if correct, show win text and +1 on array index location. if incorrect, show lose text and +1 on array index location.
 function insertQuestion() {
     // access HTML question section
     var question = document.querySelector(".question");
+    // possibly put for loop in IF
+    for (var i = 0; i < questionBank.length; i++) {
+        
+    }
+    // question.textContent = question1.question; // make this into an array question bank that grabs the object properties
     var responsesBox = document.querySelector(".responses");
+    
     var responseA = document.querySelector(".response-A");
+    responseA = question1.responseA
+    
     var responseB = document.querySelector(".response-B");
+    
     var responseC = document.querySelector(".response-C");
+    
     var responseD = document.querySelector(".response-D");
 
 }
 
+// call insertQuestion and other things? maybe use eventListener in an IF STATEMENT to verify if user selects correct response
 function quiz() {
-
+    insertQuestion();
 }
+
+// connects audio to JS
+const boop = document.getElementById("boop");
+boop.volume = 0.2;
 
 // START triggers game, timer, and show/hide pages
 startButton.addEventListener("click", function(event) {
